aceeb07d784e4a65c27ea164345348e0fa7bb3c2
Sharynne Azhar <sharynneazhar@gmail.com>
2017-07-17 22:01:29 -0500

    fix main.js

--- js/main.js
+++ js/main.js
@@ -6,10 +6,8 @@ sitemap:
 
 $(document).ready(function () {
   $('a.blog-button').click(function (e) {
-    if ($('.panel-cover').hasClass('panel-cover--collapsed')) {
-      return currentWidth = $('.panel-cover').width();
-    }
-
+    if ($('.panel-cover').hasClass('panel-cover--collapsed')) return
+    currentWidth = $('.panel-cover').width()
     if (currentWidth < 960) {
       $('.panel-cover').addClass('panel-cover--collapsed');
       $('.content-wrapper').addClass('animated slideInRight');
